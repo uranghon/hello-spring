7977fd835a6f7776ef6e7fd2e10980afb14eaf1e
uranghon <wprkffid2tp@gmail.com>
2022-12-26 11:54:21 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") // 웹어플에서 슬래쉬 hello = /hello 가 들어오면 밑에 헬로문을 호출한다??
+    public String hello(Model model)
+    {
+        model.addAttribute("data", "메건!!");
+        return "fromis";
+    }
+
+    @GetMapping("fromis")
+    public String fromis(Model model)
+    {
+        model.addAttribute("data", "하냥!!");
+        return "fromis";
+    }
+}
