33a5ba30fd0172b5e32738f0b4b86e27bb544c41
uranghon <wprkffid2tp@gmail.com>
2022-12-28 00:25:28 +0900

    공부중

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -27,4 +28,31 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 바디부에 밑에 return 값을 바로 넣어주겠다. 즉 페이지소스보기 했을때 return 값이 바로 보임.
+    public String hellostring(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name)    {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    } // JSON 방식
+    static class Hello
+    {
+        private  String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    } // 게터세터. 자바빈 표준방식. 프로퍼티 방식
+
 }
